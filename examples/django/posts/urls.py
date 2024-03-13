a01490ef7d123c552e44f079031f4992aac66ad8
viktoriapavlej <pavlejviki@gmail.com>
2024-03-13 17:59:44 +0000

    Update Restless library for Django 4 compatibility by replacing url with path and re_path

--- examples/django/posts/urls.py
+++ examples/django/posts/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import re_path, include
 
 from .api import PostResource
 
 
 urlpatterns = [
-    url(r'^posts/', include(PostResource.urls())),
+    re_path(r'^posts/', include(PostResource.urls())),
 
     # Alternatively, if you don't like the defaults...
     # url(r'^posts/$', PostResource.as_list(), name='api_posts_list'),
